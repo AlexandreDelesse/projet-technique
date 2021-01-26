76011b1aeed4b2ca6e148a35a75bf0ae100c7bdc
mahdiabderraouf <mahdi.abderraouf@outlook.com>
2021-01-26 20:40:42 +0100

    Removed submodules

---
+++ campagnes/database/migrations/2019_12_06_191000_create_campaigns_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCampaignsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('campaigns', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->text('descritption');
+            $table->text('location');
+            $table->timestamp('start_at')->nullable();
+            $table->timestamp('end_at')->nullable();
+            $table->unsignedInteger('capacity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('compagnes');
+    }
+}
---
+++ client/src/views/Home.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1>test</h1>
+
+  <font-awesome-icon icon="user-secet"></font-awesome-icon>
+</template>
